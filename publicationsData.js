acc443b7fc0278d8f98da1a099d1a4e68411a28b
Vittorio <51192576+vittorioguida@users.noreply.github.com>
2025-05-28 16:54:27 +0200

    new file:   files/CV/GUIDA_CV_May_2025.pdf 	renamed:    images/vittorio_guida.jpg -> images/Index/vittorio_guida.jpg 	renamed:    images/futures.jpg -> images/Research/futures.jpg 	renamed:    images/jeic.jpg -> images/Research/jeic.jpg 	renamed:    images/jeod.jpg -> images/Research/jeod.jpg 	modified:   index.html 	modified:   publicationsData.js

--- publicationsData.js
+++ publicationsData.js
@@ -9,7 +9,7 @@ const publications = [
         year: 2018,
         abstract: 'One of the central aspects of the Italian health-care reform deals with the new position of a chief executive officer (CEO) selected through an appointment procedure. By drawing data from 164 Curriculum Vitae (CVs), a cluster analysis was carried out to investigate the typologies of CEOs that were selected and assess them by comparing their educational and professional characteristics. Three different categories of CEOs are defined and ranked: (i) “experienced CEO”, with the highest inter-organizational mobility and the highest number of triad-assignments; (ii) “experienced local directors”, with a better cultural background, medium mobility and medium number of triad assignments; and (iii) “business like professionals” with the lowest professional and educational profile. How these typologies are distributed over the Italian regions gathered in groups were then verified, considering the kind of organizational arrangements in the local appointment procedures. Finally, the overall results of the CEOs selection system in health-care lead to suboptimal appointments: when the organizational arrangements are better employed, they totally exclude the selection of the lowest category. On the contrary, when organizational arrangements are wrongly employed the first category decreases where the second increases',
         link: 'https://jeodonline.com/jeod_articles/is-the-appointee-procedure-a-real-selection-or-a-mere-political-exchange-the-case-of-the-italian-health-care-chief-executive-officers/',
-        image: 'images/jeod.jpg'
+        image: 'images/Research/jeod.jpg'
     }, // <-- Qui mancava la chiusura dell'oggetto e la virgola
     {
         id: 'pub2',
@@ -21,7 +21,7 @@ const publications = [
         year: 2024,
         abstract: 'Prominent research in strategic imitation, exploration, exploitation, and organizational learning identifies imitation as a less costly alternative to experimentation. Yet, its role in the exploration–exploitation dilemma remains underexplored in the literature. This study employs an agent-based model to examine how two distinct agent types—those who imitate and those who experiment—interact and influence each other. The model incorporates the concept of “satisficing” derived from the behavioral theory of the firm, along with insights from research on imitative heuristics. The findings reveal that overcrowding affects both agent types negatively. Imitators suffer from diminished performance due to intensified competition, which increases as more imitators join the system. Meanwhile, explorers are hindered in their attempts at radical innovation due to the presence of other explorers and clusters of imitators. This paper contributes to the field as the first to model individual agents as ‘satisficers’ within a competitive exploration–exploitation framework. By incorporating imitation, it provides novel insights into the dynamics of organizational learning and strategic decision-making.',
         link: 'https://link.springer.com/article/10.1007/s11403-024-00406-2',
-        image: 'images/jeic.jpg'
+        image: 'images/Research/jeic.jpg'
     },
     {
         id: 'pub3',
@@ -33,7 +33,7 @@ const publications = [
         year: 2025,
         abstract: 'Nowadays we are all aware of the deep and long-lasting negative impacts of climate change. However, our existence is inherently dominated by our present circumstances, which affect our ability to preserve environmental resources – essential to leave unaltered the opportunities of future generations. Therefore, there is a tension, and decisions considered optimal for us today may not be consistent with sustainability between generations. We investigate this context of intergenerational tension across three experimental manipulations that aim to represent just as many different narratives of the future. The data collected from 180 participants show that it is hard to achieve collective sustainable outcomes by means of mere individual actions with no support of institutional actors. Consistently with previous findings, our study contributes to the literature by offering interesting policy implications on how institutionalized agencies appointed to provide advisory intergenerational guidance can act as catalysts among economic agents (e.g., individuals, but also firms and organizations), helping to overcome short-termism and generate more sustainable futures. However, in contrast with previous experimental studies, we find support that institutional enforcement is not an indispensable feature for these agencies to achieve intergenerational sustainability. Instead, institutionalized agencies can effectively promote sustainable futures based on a soft intergenerational advice.',
         link: 'https://www.sciencedirect.com/science/article/pii/S0016328724002167',
-        image: 'images/futures.jpg'
+        image: 'images/Research/futures.jpg'
     },
     {
         id: 'wp1',
