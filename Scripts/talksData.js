a1528197352e07c8eeb91853050b0f02c0c5faad
Vittorio <51192576+vittorioguida@users.noreply.github.com>
2025-10-23 17:08:15 +0200

    modified:   Scripts/talksData.js 	modified:   images/Research/Mittone_PRIN_overlap.png 	new file:   images/Talks/giuri_trento.png 	new file:   images/Talks/padova_2025.png

--- Scripts/talksData.js
+++ Scripts/talksData.js
@@ -1,48 +1,74 @@
 // talksData.js
 window.TALKS = [
 
-{
-    title: "Can AI Curb the Disposition Effect? An Experimental Study with ChatGPT",
-    event: "Workshop: Advancing Human–AI Synergy in Decision-Making and Management Innovation",
-    venue: "LUT Business School",
-    date: "2025-08-27",
-    location: "Lappeenranta, Finland",
-    desc: "Together with Matteo Benuzzi, we gave a talk on how ChatGPT may help investors overcome the disposition effect.",
-    long_desc: `Matteo Benuzzi and I will present our joint work with Azzurra Morreale and Luigi Mittone exploring whether Large Language Models (LLMs), specifically ChatGPT, can mitigate the disposition effect.`,
-    img: "images/Talks/Finland2025.jpg",
-    featured: true
-},
-{
-    title: "Two Experiments on Intergenerational Decision-Making",
-    event: "Society for the Advancement of Behavioral Economics (SABE) Annual Meeting",
-    venue: "University of Trento",
-    date: "2025-06-05",
-    location: "Trento, Italy",
-    desc: "Presented two experimental designs on intergenerational decision-making from two different studies I am conducting",
-    long_desc: "This presentation at the Society for the Advancement of Behavioral Economics (SABE) 2025 Annual Meeting focused on two innovative experimental designs aimed at understanding intergenerational decision-making processes. The first study explored how individuals make decisions that affect future generations, while the second study examined the role of social norms in shaping these decisions.",
-    img: "images/Talks/SABE2025.jpg",
-    featured: true
-},
-{
-    title: "Behavioral Insights from Resource Use Experiments: Implications for Sustainability",
-    event: "Symposium on Advancing Sustainability Research with Experimental Methods",
-    venue: "University of Padua",
-    date: "2024-12-06",
-    location: "Padua, Italy",
-    desc: "Presented findings from an experimental study the interplay between decision-making and sustainability.",
-    long_desc: `I presented experimental work on how individuals manage common-pool resources when future generations are affected. The study highlights the role of social norms and framing in promoting sustainable use, and draws lessons for designing policies that encourage long-term cooperation.`,
-    img: "images/Talks/padova2024.JPG",
-    featured: true
-},
-{
-    title: "Governance e Sostenibilità Ambientale: un laboratorio sperimentale per le organizzazioni trentine",
-    event: "Presentazione Risultati Progetto",
-    venue: "Palazzo Roccabruna",
-    date: "2024-11-15",
-    location: "Trento, Italy",
-    desc: "Along with Dr. Klaudijo Klaser and Professor Luigi Mittone, we presented the final results of a two-years research project on governance and environmental sustainability.",
-    long_desc: `On this occasion, Dr. Klaudijo Klaser, Professor Luigi Mittone, and I presented the final results of a research project on governance and environmental sustainability funded by the CARITRO Foundation and The Chamber of Commerce of Trento.`,
-    img: "images/Talks/ccia.jpeg",
-    featured: true
-},
+    {
+        title: "UPCOMING: Intertemporal Exploitation of Common-Pool Resources: An Experimental Study",
+        event: "II Symposium on Advancing Sustainability Research with Experimental Methods",
+        venue: "University of Padova",
+        date: "December 2025",
+        location: "Padova, Italy",
+        desc: "I will present the work with Sandro Casal and Luigi Mittone concerning an experimental study of common-pool resource management across overlapping generations.",
+        long_desc: `I will present the work with Sandro Casal and Luigi Mittone concerning an experimental study of common-pool resource management across overlapping generations.`,
+        img: "images/Talks/padova_2025.png",
+        featured: true
+    },
+
+    {
+        title: "UPCOMING:  Metodi Sperimentali negli studi economici",
+        event: "Analisi economica del diritto: concetti, teorie e metodi",
+        venue: "Palazzo di Giurisprudenza, University of Trento, Trento, Italy",
+        date: "2025-11-07",
+        location: "Trento, Italy",
+        desc: "I will give a presentation on experimental methods in Economics research within a broad two-days event on economic analysis of law.",
+        long_desc: `I will give a presentation on experimental methods in Economics research within a broad two-days event on economic analysis of law.`,
+        img: "images/Talks/giuri_trento.png",
+        featured: true
+    },
+
+
+    {
+        title: "Can AI Curb the Disposition Effect? An Experimental Study with ChatGPT",
+        event: "Workshop: Advancing Human–AI Synergy in Decision-Making and Management Innovation",
+        venue: "LUT Business School",
+        date: "2025-08-27",
+        location: "Lappeenranta, Finland",
+        desc: "Together with Matteo Benuzzi, we gave a talk on how ChatGPT may help investors overcome the disposition effect.",
+        long_desc: `Matteo Benuzzi and I will present our joint work with Azzurra Morreale and Luigi Mittone exploring whether Large Language Models (LLMs), specifically ChatGPT, can mitigate the disposition effect.`,
+        img: "images/Talks/Finland2025.jpg",
+        featured: true
+    },
+
+    {
+        title: "Two Experiments on Intergenerational Decision-Making",
+        event: "Society for the Advancement of Behavioral Economics (SABE) Annual Meeting",
+        venue: "University of Trento",
+        date: "2025-06-05",
+        location: "Trento, Italy",
+        desc: "Presented two experimental designs on intergenerational decision-making from two different studies I am conducting",
+        long_desc: "This presentation at the Society for the Advancement of Behavioral Economics (SABE) 2025 Annual Meeting focused on two innovative experimental designs aimed at understanding intergenerational decision-making processes. The first study explored how individuals make decisions that affect future generations, while the second study examined the role of social norms in shaping these decisions.",
+        img: "images/Talks/SABE2025.jpg",
+        featured: false
+    },
+    {
+        title: "Behavioral Insights from Resource Use Experiments: Implications for Sustainability",
+        event: "Symposium on Advancing Sustainability Research with Experimental Methods",
+        venue: "University of Padova",
+        date: "2024-12-06",
+        location: "Padova, Italy",
+        desc: "Presented findings from an experimental study the interplay between decision-making and sustainability.",
+        long_desc: `I presented experimental work on how individuals manage common-pool resources when future generations are affected. The study highlights the role of social norms and framing in promoting sustainable use, and draws lessons for designing policies that encourage long-term cooperation.`,
+        img: "images/Talks/padova2024.JPG",
+        featured: false
+    },
+    {
+        title: "Governance e Sostenibilità Ambientale: un laboratorio sperimentale per le organizzazioni trentine",
+        event: "Presentazione Risultati Progetto",
+        venue: "Palazzo Roccabruna",
+        date: "2024-11-15",
+        location: "Trento, Italy",
+        desc: "Along with Dr. Klaudijo Klaser and Professor Luigi Mittone, we presented the final results of a two-years research project on governance and environmental sustainability.",
+        long_desc: `On this occasion, Dr. Klaudijo Klaser, Professor Luigi Mittone, and I presented the final results of a research project on governance and environmental sustainability funded by the CARITRO Foundation and The Chamber of Commerce of Trento.`,
+        img: "images/Talks/ccia.jpeg",
+        featured: false
+    },
 ];
